288dd204e04cd5b2bf668258de64253443a02542
Aury <abwashi@hawk.iit.edu>
2017-03-02 21:00:25 -0600

    Added clearform() function in js

--- js/site.js
+++ js/site.js
@@ -1,13 +1,23 @@
-	$(document).ready(function() {
-		$('#gh-form').on('submit', function(e) {
-			e.preventDefault();
-			var userName = $('#name').val();
-			greetUser(userName);
+// Tell jQuery to give up the dollar sign
+$.noConflict();
 
-		});
+// jQuery 3.x-style ready event and locally scoped $
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+  $('#gh-form').on('submit', function(e) {
+   e.preventDefault();
+   var userName = $('#name').val();
+   clearForm();
+   greetUser(userName);
+ });
 
-		function greetUser(usname){
-			var greeting='<ul><li>Hi, '+usname+'!</li><li>Thank you for signing up!</li></ul>';
-			$('#greetings').append(greeting);
-		}
-	});
+  function clearForm(){
+       ('body').removeClass('page');
+  }
+
+  function greetUser(usname){
+   var greeting='<ul><li>Hi, '+usname+'!</li><li>Thank you for signing up!</li></ul>';
+   $('#greetings').append(greeting);
+ }
+});
